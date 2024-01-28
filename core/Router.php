5d114222fe7c14bd765f341586156918aeddb754
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-28 18:04:57 -0500

    feature: Router to static classs

--- core/Router.php
+++ core/Router.php
@@ -5,17 +5,17 @@ namespace core;
 use exceptions\SystemError;
 
 class Router {
-    protected $routes = []; // stores routes
+    protected static $routes = []; // stores routes
 
-    public function addRoute(string $method, string $url, callable $target) {
-        $this->routes[$method][$url] = $target;
+    public static function addRoute(string $method, string $url, callable $target) {
+        self::$routes[$method][$url] = $target;
     }
 
-    public function matchRoute() {
+    public static function matchRoute() {
         $method = $_SERVER['REQUEST_METHOD'];
         $url = $_SERVER['REQUEST_URI'];
-        if (isset($this->routes[$method])) {
-            foreach ($this->routes[$method] as $routeUrl => $target) {
+        if (isset(self::$routes[$method])) {
+            foreach (self::$routes[$method] as $routeUrl => $target) {
                 $pattern = preg_replace('/\/:([^\/]+)/', '/(?P<$1>[^/]+)', $routeUrl);
                 if (preg_match('#^' . $pattern . '$#', $url, $matches)) {
                     $params = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY); // Only keep named subpattern matches
--- routs.php
+++ routs.php
@@ -1,9 +1,7 @@
 <?php
-/**
- * @var \core\Router $router
- */
+use core\Router;
 
-$router->addRoute('GET', '/run_currents', function ($blogID) {
+Router::addRoute('GET', '/run_currents', function ($blogID) {
     echo "My route is working with blogID => $blogID !";
     exit;
 });
