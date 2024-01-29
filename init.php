e8a2d71a6c52e007453b7ec2d6f59cbe9ca5b325
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-28 19:19:33 -0500

    bugfix: bad function for spl_autoload_register

--- init.php
+++ init.php
@@ -4,10 +4,9 @@ require_once "core/procedures.php";
 
 setEnv();
 
-spl_autoload_register(function($className)
-{
-    $namespace=str_replace("\\","/",__NAMESPACE__);
-    $className=str_replace("\\","/",$className);
-    $class= "/".(empty($namespace)?"":$namespace."/")."{$className}.class.php";
+spl_autoload_register(function($className) {
+    $namespace = str_replace("\\", "/", __NAMESPACE__);
+    $className = str_replace("\\", "/", $className);
+    $class = (empty($namespace) ? "" : $namespace . "/") . "{$className}.php";
     include_once($class);
 });
